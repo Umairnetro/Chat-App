c8bbf5e4c0d52f77a21e3d5d4e0abc55570156a6
muhammadareesh <147326670+Umairnetro@users.noreply.github.com>
2024-11-16 03:07:01 +0500

    sign up functionality

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,41 +1,52 @@
 import app from "../configs/firebase";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
 import styles from "./SignUp&Login.module.css";
 
 const signUp = () => {
-const [showLoader, setShowLoader] = useState(true);
+  const [createEmail, setCreateEmail] = useState("");
+  const [createPassword, setCreatePassword] = useState("");
+  const [showLoader, setShowLoader] = useState(false);
 
   const auth = getAuth(app);
-
   const handleSignUp = () => {
+    setShowLoader(true);
     try {
-      createUserWithEmailAndPassword(auth, "UmairNew@gmail.com", "password")
-        .then((userCredential) => {
+      createUserWithEmailAndPassword(auth, createEmail, createPassword).then(
+        (userCredential) => {
           // Signed up
           const user = userCredential.user;
           console.log(user);
-        })
+          alert("Successfully Signed Up");
+        }
+      );
     } catch (error) {
       console.log(error);
-      
+      alert(error);
+    } finally {
+      setShowLoader(false);
     }
   };
 
   return (
     <>
-    {showLoader && <div className={styles.loader}></div>}
+      {showLoader && (
+        <div className="absolute w-screen h-screen bg-zinc-950 opacity-85 flex justify-center items-center">
+          <div className={styles.loader}></div>
+        </div>
+      )}
       <div
         className={`flex flex-col items-center gap-4 bg-white p-4 rounded-lg w-1/4`}
       >
         <input
+          onChange={(e) => setCreateEmail(e.target.value)}
           className="bg-zinc-300 placeholder:text-zinc-500 rounded-full px-3 py-2 text-gray-800 outline-none border-2 border-zinc-400 focus:border-teal-800 duration-500 w-full"
           type="email"
           placeholder="Email"
         />
         <input
+          onChange={(e) => setCreatePassword(e.target.value)}
           className="bg-zinc-300 placeholder:text-zinc-500 rounded-full px-3 py-2 text-gray-800 outline-none border-2 border-zinc-400 focus:border-teal-800 duration-500 w-full"
           type="text"
           placeholder="Password"
